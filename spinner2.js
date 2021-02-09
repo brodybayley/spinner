5455022d138a81842e159eb70140b5f4f3ef0ec3
Brody Bayley <brodybayley@gmail.com>
2021-02-09 23:31:06 +0000

    pushing through final changes for spinner2.js

--- spinner2.js
+++ spinner2.js
@@ -1,61 +1,12 @@
-process.stdout.write('hello from spinner1.js... \rheyyy\n');
-
 const finalString = () => {
-  let count = 0;
-  for (const char of sentence) {
+  let moves = ['\r|   ', '\r/   ', '\r-   ', '\r\\    ', '\r|   ', '\r/   ', '\r-   ', '\r\\    ', '\r|   '];
+  let count = 100;
+  for (const char of moves) {
     setTimeout(() => {
       process.stdout.write(char);
-    }, 100);
+    }, count);
     count = count + 200;
   }
+};
 
-  setTimeout(() => {
-    process.stdout.write('\r|   ');
-  }, 100);
-
-  setTimeout(() => {
-    process.stdout.write('\r/   ');
-  }, 300);
-
-  setTimeout(() => {
-    process.stdout.write('\r-   ');
-  }, 500);
-
-  setTimeout(() => {
-    process.stdout.write('\r\\    ');
-  }, 700);
-
-  setTimeout(() => {
-    process.stdout.write('\r|   ');
-  }, 900);
-
-  setTimeout(() => {
-    process.stdout.write('\r/   ');
-  }, 1100);
-
-  setTimeout(() => {
-    process.stdout.write('\r-   ');
-  }, 1300);
-
-  setTimeout(() => {
-    process.stdout.write('\r\\    ');
-  }, 1500);
-
-  setTimeout(() => {
-    process.stdout.write('\r|   ');
-  }, 1700);
-
-
-  const finalString = () => {
-    let count = 0;
-    for (const char of sentence) {
-      setTimeout(() => {
-        process.stdout.write(char);
-      }, count * 50);
-      count++;
-    }
-
-    setTimeout(() => {
-      process.stdout.write('\n');
-    }, sentence.length * 50);
-  };
+finalString();
